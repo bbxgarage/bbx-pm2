c6ab0506dcb89fbb7972fd32c96a66e604f8d5dc
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2018-07-23 17:42:42 -0300

    add key

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
-    name: 'API',
-    script: 'app.js',
+    name      : 'API',
+    script    : 'app.js',
     env: {
       NODE_ENV: 'development'
     },
@@ -12,13 +12,13 @@ module.exports = {
 
   deploy : {
     production : {
-      key: "/root/deploy-key.pem",
-      user: 'root',
-      host: '159.65.46.80',
-      ref : 'origin/master',
-      repo: 'git@github.com:bbxgarage/bbx-pm2.git',
-      path: '/var/www/production',
-      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
+      key: '/root/github_rsa',
+      user : 'root',
+      host : '159.65.46.80',
+      ref  : 'origin/master',
+      repo : 'git@github.com:EBDS-TECNOLOGIA/Zuni.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
     }
   }
 };
