c68c6d1694c2b1b5bf9adcd1b0b65f93eaaf10a5
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2018-07-23 15:04:00 -0300

    init app

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps : [{
+    name: 'API',
+    script: 'app.js',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production : {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user: 'root',
+      host: '159.65.46.80',
+      ref : 'origin/master',
+      repo: 'git@github.com:bbxgarage/bbx-pm2.git',
+      path: '/var/www/production',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
